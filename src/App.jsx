9e4476c9185f7dcb996abbe08a10552facbae05f
Chloe <c.williams22@hotmail.co.uk>
2019-08-06 16:41:00 +0100

    styles the login page using material ui

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,17 @@
 import React from 'react';
 import { Router } from '@reach/router';
-import Header from './components/Header';
+// import Header from './components/Header';
 import Login from './components/Login';
 import RegForm from './components/RegForm';
 import Upload from './components/Upload';
-
 import './App.css';
 
 function App() {
+
   return (
-    <div className="App">
-      <Header />
-      <Router className="main">
+    <div className="App" >
+      {/* <Header /> */}
+      <Router>
         <Login path='/login' />
         <RegForm path='/register' />
         <Upload path='/upload' />
--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,86 +1,53 @@
-import React, { Component } from 'react';
-import Avatar from '@material-ui/core/Avatar';
-import Button from '@material-ui/core/Button';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import TextField from '@material-ui/core/TextField';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
-import Checkbox from '@material-ui/core/Checkbox';
-import Link from '@material-ui/core/Link';
-import Paper from '@material-ui/core/Paper';
-import Box from '@material-ui/core/Box';
-import Grid from '@material-ui/core/Grid';
+import React, { Component } from "react";
+import TextField from "@material-ui/core/TextField";
+import Link from "@material-ui/core/Link";
+import Paper from "@material-ui/core/Paper";
+import Grid from "@material-ui/core/Grid";
 // import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
-import Typography from '@material-ui/core/Typography';
-import { makeStyles } from '@material-ui/core/styles';
-import '../App.css';
+import Typography from "@material-ui/core/Typography";
+import { makeStyles } from "@material-ui/core/styles";
+import "../App.css";
+import "../styles/Login.css"
 
 class Login extends Component {
-  state = {}
+  state = {};
   render() {
     return (
-    <Grid container component="main" className='root'>
-    <CssBaseline />
-    <Grid item xs={false} sm={4} md={7} className='image' />
-    <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
-      <div className='paper'>
-        {/* <Avatar className={classes.avatar}>
-          <LockOutlinedIcon />
-        </Avatar> */}
-        <Typography component="h1" variant="h5">
-          Sign in
-        </Typography>
-        <form className='form' noValidate>
-          <TextField
-            variant="outlined"
-            margin="normal"
-            required
-            fullWidth
-            id="email"
-            label="Email Address"
-            name="email"
-            autoComplete="email"
-            autoFocus
-          />
-          <TextField
-            variant="outlined"
-            margin="normal"
-            required
-            fullWidth
-            name="password"
-            label="Password"
-            type="password"
-            id="password"
-            autoComplete="current-password"
-          />
-          <FormControlLabel
-            control={<Checkbox value="remember" color="primary" />}
-            label="Remember me"
-          />
-          <Button
-            type="submit"
-            fullWidth
-            variant="contained"
-            color="primary"
-            className='submit'
-          >
-            Sign In
-          </Button>
-          <Grid container>
-            <Grid item xs>
-              <Link href="#" variant="body2">
-                Forgot password?
-              </Link>
-            </Grid>
-            <Grid item>
-              <Link href="#" variant="body2">
-                {"Don't have an account? Sign Up"}
-              </Link>
-            </Grid>
-          </Grid>
-        </form>
-      </div>
-    </Grid>
-  </Grid>);
+      <Grid container component="main" className="root">
+        {/* <CssBaseline /> */}
+        <Grid item xs={false} sm={4} md={7} className="image" />
+        <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
+          <div className="paper">
+            <form className="form" noValidate>
+              <div className='group'>
+                <input type="text" />
+                <label>Username</label>
+                <span className="highlight" />
+                <span className="bar" />
+              </div>
+
+              <div className='group'>
+                <input type="password" />
+                <label>Password</label>
+                <span className="highlight" />
+                <span className="bar" />
+              </div>
+
+              <button type="submit" className="submit">
+                SIGN IN
+                <div className="ripples">
+                  <span className="circle"></span>
+                </div>
+              </button>
+              <div className="links">
+                <Link to="#">Forgot password?</Link>
+                <Link to="#">Don't have an account? Sign Up</Link>
+              </div>
+            </form>
+          </div>
+        </Grid>
+      </Grid>
+    );
   }
 }
 
