742d79dd20c2210a82f06b77519c09cc297f87ce
Chloe <c.williams22@hotmail.co.uk>
2019-08-07 10:10:38 +0100

    adds handleChange and handleSubmit to login

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -4,25 +4,30 @@ import Paper from "@material-ui/core/Paper";
 import Grid from "@material-ui/core/Grid";
 import "../App.css";
 import "../styles/Login.css"
+import { thisExpression } from "@babel/types";
 
 class Login extends Component {
-  state = {};
+  state = {
+    username: '',
+    password: ''
+  };
+
   render() {
     return (
       <Grid container component="main" className="root">
         <Grid item xs={false} sm={4} md={7} className="image" />
         <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
           <div className="paper">
-            <form className="form" noValidate>
+            <form className="form" onSubmit={this.handleSubmit}>
               <div className='group'>
-                <input type="text" />
+                <input type="text" onChange={this.handleChange} value={this.state.username}/>
                 <label>Username</label>
                 <span className="highlight" />
                 <span className="bar" />
               </div>
 
               <div className='group'>
-                <input type="password" />
+                <input type="password" onChange={this.handleChange} value={this.state.password}/>
                 <label>Password</label>
                 <span className="highlight" />
                 <span className="bar" />
@@ -43,6 +48,18 @@ class Login extends Component {
         </Grid>
       </Grid>
     );
+  }
+
+  handleChange = event => {
+    const { value } = event.target;
+    this.setState({username: value})
+  };
+
+  handleSubmit = event => {
+    event.preventDefault();
+    const {username} = this.state;
+    // api request needed here?
+    this.setState({username: '', password: ''})
   }
 }
 
