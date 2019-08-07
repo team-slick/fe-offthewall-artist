3bcf7e2b2bd3d73f3f7fb260d6371ce21548b68f
Chloe <c.williams22@hotmail.co.uk>
2019-08-07 08:34:26 +0100

    breaks out styling for each component

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,11 +1,7 @@
 import React, { Component } from "react";
-import TextField from "@material-ui/core/TextField";
 import Link from "@material-ui/core/Link";
 import Paper from "@material-ui/core/Paper";
 import Grid from "@material-ui/core/Grid";
-// import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
-import Typography from "@material-ui/core/Typography";
-import { makeStyles } from "@material-ui/core/styles";
 import "../App.css";
 import "../styles/Login.css"
 
@@ -14,7 +10,6 @@ class Login extends Component {
   render() {
     return (
       <Grid container component="main" className="root">
-        {/* <CssBaseline /> */}
         <Grid item xs={false} sm={4} md={7} className="image" />
         <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
           <div className="paper">
