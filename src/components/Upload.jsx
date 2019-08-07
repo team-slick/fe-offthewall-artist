45408ddd5546f1c1dcfce561e69068cae6e12a99
Chloe <c.williams22@hotmail.co.uk>
2019-08-07 14:40:49 +0100

    retrieves walls from the db and implements firebase upload

--- src/components/Upload.jsx
+++ src/components/Upload.jsx
@@ -1,10 +1,100 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import Paper from "@material-ui/core/Paper";
+import Grid from "@material-ui/core/Grid";
+import { storage } from "../firebase";
+import "../App.css";
+import "../styles/Login.css";
+import { Query } from '@apollo/react-components';
+import { gql } from 'apollo-boost';
 
 class Upload extends Component {
-  state = {}
+  state = {
+    image: null,
+    urlString: ""
+  };
+
   render() {
-    return <p>upload</p>;
+    return (
+      <Grid container component="main" className="root">
+        <Grid item xs={false} sm={4} md={7} className="image" />
+        <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
+          <div className="paper">
+            <p>
+              Welcome (enter username here), simply choose a wall from the list
+              below and upload you art.
+            </p>
+            <form className="form">
+              <select>
+                <Query query={gql`
+                {
+                  fetchAllWalls {
+                    wall_id
+                    street_address
+                    canvas_width
+                    canvas_height
+                  }
+                }`}>
+                {({loading, error, data }) => {
+                  if (loading) return <option>Loading...</option>
+                  if (error) return <option>Error :(</option>
+                  return data.fetchAllWalls.map(wall => (
+                    <option value={wall.street_address} key={wall.wall_id}>Address: {wall.street_address}  Dimensions: {wall.canvas_width} x {wall.canvas_height}</option>
+                  ))
+                }}
+                </Query>
+              </select>
+              <input type="file" onChange={this.handleChange} />
+              <button
+                className="submit"
+                onClick={this.handleUpload}
+              >
+                SUBMIT
+                <div className="ripples">
+                  <span className="circle" />
+                </div>
+              </button>
+            </form>
+          </div>
+        </Grid>
+      </Grid>
+    );
   }
+
+  componentDidMount = () => {
+  }
+
+  handleChange = event => {
+    if (event.target.files[0]) {
+      const image = event.target.files[0];
+      this.setState(() => ({ image }));
+    }
+  };
+
+  handleUpload = (event) => {
+    event.preventDefault()
+    const { image } = this.state;
+    const uploadTask = storage.ref(`${image.name}`).put(image);
+    uploadTask.on(
+      "state_changed",
+      snapshot => {
+        // progress function
+      },
+      error => {
+        // error function
+        console.log(error);
+      },
+      () => {
+        // complete function
+        storage
+          .ref()
+          .child(image.name)
+          .getDownloadURL()
+          .then(url => {
+            this.setState({urlString: url})
+          })
+      }
+    );
+  };
 }
 
 export default Upload;
---
+++ src/firebase/index.js
@@ -0,0 +1,18 @@
+import firebase from "firebase/app";
+import "firebase/storage";
+
+const config = {
+ apiKey: "AIzaSyDlLd0P5oDYnOtmDqvZyDdVUha9rhqiEc0",
+ authDomain: "slickproject-fbaad.firebaseapp.com",
+ databaseURL: "https://slickproject-fbaad.firebaseio.com",
+ projectId: "slickproject-fbaad",
+ storageBucket: "slickproject-fbaad.appspot.com",
+ messagingSenderId: "375260658649",
+ appId: "1:375260658649:web:50d2dbee6d88dabf"
+};
+
+firebase.initializeApp(config);
+
+const storage = firebase.storage();
+
+export { storage, firebase as default };
