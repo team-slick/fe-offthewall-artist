30040d958dbf16a23f2af6cc1f4b157abb85def0
Luke Rushworth <ldjrushworth@icloud.com>
2019-08-08 12:05:24 +0100

    commits working login mutation

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -6,22 +6,8 @@ import "../App.css";
 import "../styles/Login.css";
 // import { thisExpression } from "@babel/types";
 import { gql } from "apollo-boost";
-import { Mutation, Query } from "@apollo/react-components";
-
-//hard-coded for now
-const AUTH_TOKEN =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwidXNlcm5hbWUiOiJib2JiaXJhZSIsImlhdCI6MTU2NTE4NTE3OCwiZXhwIjoxNTY3Nzc3MTc4fQ.4eUZhr64Om072dYartWGpcnoai9UZonskHajuH6rfzU";
-
-// const LOGIN = gql`
-//   mutation($artist_username: String!, $artist_password: String!) {
-//     login(
-//       artist_username: $artist_username
-//       artist_password: $artist_password
-//     ) {
-//       token
-//     }
-//   }
-// `;
+import { Mutation } from "@apollo/react-components";
+import { navigate } from "@reach/router";
 
 const LOGIN = gql`
   mutation Login($artist_username: String!, $artist_password: String!) {
@@ -39,8 +25,8 @@ const LOGIN = gql`
 
 class Login extends Component {
   state = {
-    artist_username: "bobbirae",
-    artist_password: "password"
+    artist_username: "",
+    artist_password: ""
   };
 
   render() {
@@ -50,13 +36,16 @@ class Login extends Component {
         <Grid item xs={false} sm={4} md={7} className="image" />
         <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
           <div className="paper">
-            <Mutation mutation={LOGIN} onCompleted={data => console.log(data)}>
+            <Mutation
+              mutation={LOGIN}
+              onCompleted={({ login }) => {
+                this.saveUserData(login.user.artist_id, login.token);
+              }}
+            >
               {(login, { loading, error, data }) => {
                 // if (loading) return <p>Loading...</p>;
                 // if (error) return <p>Error.</p>;
                 // if (data) console.log("data");
-
-                const tokenToRender = data;
 
                 return (
                   <form
@@ -65,74 +54,9 @@ class Login extends Component {
                       event.preventDefault();
                       login({
                         variables: { artist_username, artist_password }
-                      });
-                    }}
-                  >
-                    <button type="submit">SIGN IN</button>
-                  </form>
-                );
-              }}
-            </Mutation>
-          </div>
-        </Grid>
-      </Grid>
-    );
-  }
-
-  saveUserData = token => {
-    localStorage.setItem(AUTH_TOKEN, token);
-  };
-
-  handleChange = event => {
-    const { value } = event.target;
-    if (event.target.type === "text") {
-      this.setState({ username: value });
-    } else {
-      this.setState({ password: value });
-    }
-  };
-
-  // handleSubmit = event => {
-  //   console.log('submitting')
-  //   event.preventDefault();
-  //   const { username } = this.state;
-  //   // api request needed here?
-  //   this.setState({ username: '', password: '' })
-  // }
-}
-
-export default Login;
-
-// <Mutation
-//   mutation={LOGIN}
-//   variables={{ username, password }}
-//   onCompleted={data => console.log(data)}
-// >
-//   {mutation => <div onClick={mutation}>Run mutation</div>}
-// {
-/* {(login, { loading, data }) => {
-          if (loading) console.log("loading");
-          if (data) console.log("data");
-          return (
-            <Grid container component="main" className="root">
-              <Grid item xs={false} sm={4} md={7} className="image" />
-              <Grid
-                item
-                xs={12}
-                sm={8}
-                md={5}
-                component={Paper}
-                elevation={6}
-                square
-              >
-                <div className="paper">
-                  <form
-                    className="form"
-                    onSubmit={event => {
-                      event.preventDefault();
-                      login("bobbirae", "password")
-                        .then(res => console.log(res))
-                        .catch(err => console.log(err));
+                      })
+                        .then(res => res)
+                        .catch(err => err);
                     }}
                   >
                     <div className="group">
@@ -163,26 +87,32 @@ export default Login;
                     </button>
                     <div className="links">
                       <Link to="#">Forgot password?</Link>
-                      <Link to="#">Don't have an account? Sign Up</Link>
+                      <Link to="#">Don't have an account? Sign up.</Link>
                     </div>
                   </form>
-                </div>
-              </Grid>
-            </Grid>
-          // );
-        }} */
-// }
-// </Mutation>
-// const LOGIN = gql`
-//   mutation Login($artist_username: String!, $artist_password: String!) {
-//     login(
-//       artist_username: $artist_username
-//       artist_password: $artist_password
-//     ) {
-//       token
-//       user {
-//         artist_id
-//       }
-//     }
-//   }
-// `;
+                );
+              }}
+            </Mutation>
+          </div>
+        </Grid>
+      </Grid>
+    );
+  }
+
+  saveUserData = (artist_id, token) => {
+    localStorage.setItem("ARTIST_ID", artist_id);
+    localStorage.setItem("AUTH_TOKEN", token);
+    navigate("/upload");
+  };
+
+  handleChange = event => {
+    const { value } = event.target;
+    if (event.target.type === "text") {
+      this.setState({ artist_username: value });
+    } else {
+      this.setState({ artist_password: value });
+    }
+  };
+}
+
+export default Login;
