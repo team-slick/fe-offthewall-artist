9a89e5e9d2223a475f416b448f3abd54606e698e
ian <kennaleeds@gmail.com>
2019-08-21 15:32:56 +0100

    adds account admin email link

--- src/components/Header.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import '../App.css';
-
-const Header = () => {
-  return <h className='header'>HEADER</h>;
-}
-
-
-export default Header;
--- src/components/RegForm.jsx
+++ src/components/RegForm.jsx
@@ -1,9 +1,17 @@
 import React, { Component } from 'react';
+import '../App.css';
 
 class RegForm extends Component {
   state = {}
   render() {
-    return <p>regform</p>;
+    return (
+      <div>
+        <p>This part of the site currently needs a little love.</p>
+        <p>If you'd like to register an account or reset a password, please send us an email!</p>
+        <a href="mailto:hi.artleeds@gmail.com">hi.artleeds@gmail.com</a>
+
+      </div>
+    )
   }
 }
 
